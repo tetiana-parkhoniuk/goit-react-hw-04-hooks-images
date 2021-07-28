c0f3050b5a5e2948898ef252385aa16e3ddc77bf
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-07-28 20:01:19 +0300

    Rewrites App with Hooks.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import Searchbar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery ';
@@ -8,97 +8,174 @@ import Modal from 'components/Modal';
 import { fetchImages } from 'services/api';
 import styles from './App.module.css';
 
-export default class App extends Component {
-  state = {
-    searchQuery: null,
-    images: [],
-    page: 1,
-    reqStatus: 'idle',
-    selectedImage: null,
-  };
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [reqStatus, setReqStatus] = useState('idle');
+  const [selectedImage, setSelectedImage] = useState(null);
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
+  useEffect(() => {
+    if (searchQuery === '') return;
+    async function getImages() {
       try {
-        this.setState({ reqStatus: 'pending' });
-
+        setReqStatus('pending');
         const images = await fetchImages(
-          this.state.searchQuery,
-          this.state.page,
+          searchQuery,
+          page
         );
-
-        this.setState({ images, reqStatus: 'fulfilled' });
+        setImages(prevState => [...prevState, ...images]);
+        setReqStatus('fulfilled');
+        scrollOnLoadMore();
       } catch (error) {
-        this.setState({ reqStatus: 'rejected' });
-        toast.error('Something went worng, please try again later.');
-      }
-    } else if (prevState.page !== this.state.page) {
-      try {
-        this.setState({ reqStatus: 'pending' });
-
-        const images = await fetchImages(
-          this.state.searchQuery,
-          this.state.page,
-        );
-
-        this.setState({
-          images: [...this.state.images, ...images],
-          reqStatus: 'fulfilled',
-        });
-        this.scrollOnLoadMore();
-      } catch (error) {
-        this.setState({ reqStatus: 'rejected' });
+        setReqStatus('rejected');
         toast.error('Something went worng, please try again later.');
       }
     }
-  }
+    getImages();
+  }, [searchQuery, page]);
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery, page: 1, images: [] });
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
+    setImages([]);
+    setPage(1);
   };
 
-  incrementPage = () => {
-    this.setState({ page: this.state.page + 1 });
+  const incrementPage = () => {
+    setPage(prevPage => prevPage +1)
   };
 
-  scrollOnLoadMore = () => {
+  const scrollOnLoadMore = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
   };
 
-  toggleModal = () => {
-    this.setState(({ selectedImage }) => ({
-      selectedImage: !selectedImage,
-    }));
+  const toggleModal = () => {
+    setSelectedImage(!selectedImage);
   };
 
-  handleSelectedImage = imageURL => {
-    this.setState({ selectedImage: imageURL });
+  const handleSelectedImage = imageURL => {
+    setSelectedImage(imageURL);
   };
 
-  render() {
-    const { images, reqStatus, selectedImage } = this.state;
-    const isLoading = reqStatus === 'pending';
-    const showLoadMoreBtn = images.length > 0;
 
-    return (
-      <div className={styles.app}>
+  const isLoading = reqStatus === 'pending';
+  const showLoadMoreBtn = images.length > 0;
+
+  return (
+    <div className={styles.app}>
         <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
 
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSubmit={handleFormSubmit} />
 
-        {<ImageGallery images={images} onSelect={this.handleSelectedImage} />}
+        <ImageGallery images={images} onSelect={handleSelectedImage} />
 
         {isLoading && <Spinner />}
 
-        {showLoadMoreBtn && <Button onClick={this.incrementPage} />}
+        {showLoadMoreBtn && <Button onClick={incrementPage} />}
 
         {selectedImage && (
-          <Modal onClose={this.toggleModal} largeImageURL={selectedImage} />
+          <Modal onClose={toggleModal} largeImageURL={selectedImage} />
         )}
-      </div>
-    );
-  }
+    </div>
+  )
 }
+
+////==== Original Code ====////
+
+// export default class App extends Component {
+//   state = {
+//     searchQuery: null,
+//     images: [],
+//     page: 1,
+//     reqStatus: 'idle',
+//     selectedImage: null,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       try {
+//         this.setState({ reqStatus: 'pending' });
+
+//         const images = await fetchImages(
+//           this.state.searchQuery,
+//           this.state.page,
+//         );
+
+//         this.setState({ images, reqStatus: 'fulfilled' });
+//       } catch (error) {
+//         this.setState({ reqStatus: 'rejected' });
+//         toast.error('Something went worng, please try again later.');
+//       }
+//     } else if (prevState.page !== this.state.page) {
+      // try {
+      //   this.setState({ reqStatus: 'pending' });
+
+      //   const images = await fetchImages(
+      //     this.state.searchQuery,
+      //     this.state.page,
+      //   );
+
+      //   this.setState({
+      //     images: [...this.state.images, ...images],
+      //     reqStatus: 'fulfilled',
+      //   });
+      //   this.scrollOnLoadMore();
+      // } catch (error) {
+      //   this.setState({ reqStatus: 'rejected' });
+      //   toast.error('Something went worng, please try again later.');
+      // }
+//     }
+//   }
+
+//   handleFormSubmit = searchQuery => {
+//     this.setState({ searchQuery, page: 1, images: [] });
+//   };
+
+//   incrementPage = () => {
+//     this.setState({ page: this.state.page + 1 });
+//   };
+
+//   scrollOnLoadMore = () => {
+//     window.scrollTo({
+//       top: document.documentElement.scrollHeight,
+//       behavior: 'smooth',
+//     });
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ selectedImage }) => ({
+//       selectedImage: !selectedImage,
+//     }));
+//   };
+
+//   handleSelectedImage = imageURL => {
+//     this.setState({ selectedImage: imageURL });
+//   };
+
+//   render() {
+//     const { images, reqStatus, selectedImage } = this.state;
+//     const isLoading = reqStatus === 'pending';
+//     const showLoadMoreBtn = images.length > 0;
+
+//     return (
+//       <div className={styles.app}>
+//         <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
+
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+
+//         {<ImageGallery images={images} onSelect={this.handleSelectedImage} />}
+
+//         {isLoading && <Spinner />}
+
+//         {showLoadMoreBtn && <Button onClick={this.incrementPage} />}
+
+//         {selectedImage && (
+//           <Modal onClose={this.toggleModal} largeImageURL={selectedImage} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const fetchImages = async (searchQuery, page) => {
+export const fetchImages = async (searchQuery, page=1) => {
   const API_KEY = '21882924-40498065f1aa5022828b315f8';
   const params = `page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
   const response = await axios.get(`?q=${searchQuery}&${params}`);
